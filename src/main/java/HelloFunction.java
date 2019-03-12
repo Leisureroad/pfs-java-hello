a1ed062bbe3956499f8d5cc0a6fb842faad78803
leisureroad <charles_liuf@hotmail.com>
2019-03-12 11:52:08 +0800

    update again

--- src/main/java/HelloFunction.java
+++ src/main/java/HelloFunction.java
@@ -2,7 +2,7 @@ import java.util.function.Function;
 
 public class HelloFunction implements Function<String, String> {
     public String apply(String s) {
-        String result = "Hello, " + s + "!";
+        String result = "Hello, " + s + ", updated!";
         System.out.println(result);
         return result;
     }
