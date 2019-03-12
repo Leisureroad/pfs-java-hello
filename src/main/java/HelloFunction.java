d7d5f5aee840d96ec4632b9562d3f37de86a11ce
leisureroad <charles_liuf@hotmail.com>
2019-03-12 11:55:05 +0800

    update 2nd time

--- src/main/java/HelloFunction.java
+++ src/main/java/HelloFunction.java
@@ -2,7 +2,7 @@ import java.util.function.Function;
 
 public class HelloFunction implements Function<String, String> {
     public String apply(String s) {
-        String result = "Hello, " + s + ", updated!";
+        String result = "Hello, " + s + ", updated 2nd time!";
         System.out.println(result);
         return result;
     }
